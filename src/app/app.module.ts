4111e6d6184c4c0f79f0f277425d7a509337f55d
Nazarii <nfenii02@gmail.com>
2018-06-27 19:18:28 +0300

    List of users implemented search added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserListComponent } from './user-list/user-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { OrgsListComponent } from './orgs-list/orgs-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
+    UserListComponent,
+    OrgsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+
+export class UserListComponent implements OnInit {
+
+  items: number = 4;
+  userList: any = [];
+  defaultArray: any = [];
+  usersShowCount: number = 0;
+
+  constructor(private httpClient: HttpClient) { };
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  filterUser(event: any) {
+    let inputData:any = event.target.value;
+      this.userList = this.defaultArray;
+      this.userList = this.userList.filter(user=>user.login.includes(inputData));
+  }
+
+  getUsers() {
+    this.httpClient.get('https://api.github.com/users')
+      .subscribe((data: any[]) => {
+        if (data.length) {
+          this.defaultArray = data;
+          this.userList = this.defaultArray;
+        }
+        console.log(this.defaultArray);
+      })
+  }
+  getOrganizations() {
+    this.httpClient.get('https://api.github.com/organizations')
+      .subscribe((data: any[]) => {
+        if (data.length) {
+          this.defaultArray = data;
+          this.userList = this.defaultArray;
+        }
+        console.log(this.defaultArray);
+      })
+  }
+}
